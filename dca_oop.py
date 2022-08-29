7c6601428b2c0b7c04e71f6085172d17b3e22a77
Ahmed Elsayed <82623452+ellord-start@users.noreply.github.com>
2022-08-29 09:55:52 +0200

    new version based on oop

---
+++ dca_oop.py
@@ -0,0 +1,161 @@
+# only one class to deal with all methods
+import pandas as pd
+import numpy as np
+from scipy.optimize import curve_fit
+class SingleModel:
+    def __init__(self,Q,T):
+        self.Q =Q
+        self.T =T
+        self.q_max = max(Q)
+        self.T_max = max(T)
+    def normalize(self):
+        # normalize the columns
+        self.T = self.T / max(self.T)
+        self.Q = self.Q / max(self.Q)
+    def RMSE(self,y , y_fit ):
+        N = len(y)
+        return np.sqrt(np.sum(y - y_fit) ** 2 / N)
+        # De-normalize qi and di
+    def hyperbolic(self,t, qi, di, b):
+            return qi / (np.abs((1 + b * di * t)) ** (1 / b))
+
+    def exposential(self,t, qi, di):
+        return qi * np.exp(-di * t)
+
+    def harmonic(self,t, qi, di):
+        return qi / (1 + di * t)
+class Exponential(SingleModel):
+    def __init__(self,Q,T):
+        SingleModel.__init__(self,Q,T)
+    def fit(self):
+        #normalize the data
+        self.normalize()
+        popt, pcov = curve_fit(self.exposential, self.T, self.Q)
+        qi, di = popt
+        qfit_normalized = self.exposential(self.T, qi, di)
+        q_fitted = qfit_normalized * self.q_max
+        RMSE = self.RMSE(self.Q, qfit_normalized)
+        # De-normalize qi and di
+        qi = qi * self.q_max
+        di = di / self.T_max
+        parameters = [qi, di, 0, RMSE, "Exponential"]
+        return parameters , q_fitted
+class Hyperbolic(SingleModel):
+    def __init__(self,Q,T):
+        SingleModel.__init__(self,Q,T)
+    def fit(self):
+        #normalize the data
+        self.normalize()
+        popt, pcov = curve_fit(self.hyperbolic, self.T, self.Q)
+        qi, di,b = popt
+        qfit_normalized = self.hyperbolic(self.T, qi, di,b)
+        q_fitted = qfit_normalized * self.q_max
+        RMSE = self.RMSE(self.Q, qfit_normalized)
+        # De-normalize qi and di
+        qi = qi * self.q_max
+        di = di / self.T_max
+        parameters = [qi, di, b, RMSE, "Hyperbolic"]
+        return parameters , q_fitted
+class Harmonic(SingleModel):
+    def __init__(self,Q,T):
+        SingleModel.__init__(self,Q,T)
+    def fit(self):
+        #normalize the data
+        self.normalize()
+        popt, pcov = curve_fit(self.harmonic, self.T, self.Q)
+        qi, di= popt
+        qfit_normalized = self.harmonic(self.T, qi, di)
+        q_fitted = qfit_normalized * self.q_max
+        RMSE = self.RMSE(self.Q, qfit_normalized)
+        # De-normalize qi and di
+        qi = qi * self.q_max
+        di = di / self.T_max
+        parameters = [qi, di, 1, RMSE, "Harmonic"]
+        return parameters , q_fitted
+
+class ARPS:
+    """
+    Takes the productions and date columns and provide ARP's paramters for the 3 models
+    """
+    def __init__(self, dataframe : pd.DataFrame , production_col : str , date_column :str):
+        self.q = production_col
+        self.date = date_column
+        self.df = dataframe[[date_column,production_col]]
+        self.Q = 0
+        self.T =0
+        self.ex_params = None
+        self.hp_params = None
+        self.Hr_params = None
+    def smooth(self, window_size : int , stds : int , trim : bool ):
+        # smoothing using moving average
+        self.df[self.q + '_rol_Av'] = self.df[self.q].rolling(window=window_size, center=True).mean()
+        # identify the outliers and remove them
+        self.df[self.q + '_rol_Std'] = self.df[self.q].rolling(window=window_size, center=True).std()
+        self.df[self.q + '_is_Outlier'] = (abs(self.df[self.q] - self.df[self.q + '_rol_Av']) > ( stds * self.df[self.q+'_rol_Std']))
+        result = self.df.drop(self.df[self.df[self.q + '_is_Outlier'] == True].index).reset_index(drop=True)
+        # Remove rows where "_rol_Av" has NaNs
+        result = result[result[self.q + '_rol_Av'].notna()]
+        # remove the increasing part of the curve we only concern about the decline part
+        if trim == True:
+            # Trim initial buildup
+            maxi = result[self.q + '_rol_Av'].max()
+            maxi_index = (result[result[self.q + '_rol_Av'] == maxi].index.values)[0]
+            result = result.iloc[maxi_index:, :].reset_index(drop=True)
+        self.df = result
+        return self.df
+
+    def prepocess_date_col(self,frequency = "Daily"):
+        self.df[self.date] = pd.to_datetime(self.df[self.date])
+        self.df["Time [{frequency}]"] = (self.df[self.date] - self.df[self.date].iloc[0])
+        if frequency == "Daily":
+            self.df["Time [{frequency}]"] = self.df["Time [{frequency}]"] / np.timedelta64(1, 'D')
+            self.df["Time [{frequency}]"] = self.df["Time [{frequency}]"].astype(int)
+        elif frequency == "Monthly":
+            self.df["Time [{frequency}]"] = self.df["Time [{frequency}]"] / np.timedelta64(1, 'M')
+            self.df["Time [{frequency}]"] = self.df["Time [{frequency}]"].astype(int)
+        elif frequency == "Yearly":
+            self.df["Time [{frequency}]"] = self.df["Time [{frequency}]"] / np.timedelta64(1, 'Y')
+            self.df["Time [{frequency}]"] = self.df["Time [{frequency}]"].astype(int)
+        self.T = self.df["Time [{frequency}]"]
+        self.Q = self.df[self.q + '_rol_Av']
+
+    def fit_exponential(self):
+         parameters , q_fitted = Exponential(self.Q, self.T).fit()
+         self.ex_params = parameters
+         return parameters , q_fitted
+
+    def fit_hyperbolic(self):
+        parameters, q_fitted = Hyperbolic(self.Q, self.T).fit()
+        self.hp_params = parameters
+        return parameters , q_fitted
+
+    def fit_harmonic(self):
+        parameters, q_fitted = Harmonic(self.Q, self.T).fit()
+        self.Hr_params = parameters
+        return parameters , q_fitted
+
+    def fit_all_models(self):
+        ex ,qex = self.fit_exponential()
+        hp , qhp= self.fit_hyperbolic()
+        hr , qhr = self.fit_harmonic()
+        all_params = [ex, hp, hr]
+        data_info = {
+            "Model": [i[-1] for i in all_params],
+            "Qi": [i[0] for i in all_params],
+            "Di": [i[1] for i in all_params],
+            "b": [i[2] for i in all_params],
+            "Normalized RMSE": [i[3] for i in all_params],
+        }
+        Qs = pd.DataFrame({
+            "Time": self.T,
+            "Original_Smoothed": self.Q,
+            "Exponential": qex,
+            "Harmonic": qhp,
+            "Hyperbolic": qhr
+        })
+        return data_info , Qs
+
+
+
+
+
---
+++ main2.py
@@ -0,0 +1,73 @@
+import streamlit as st
+import pandas as pd
+import plotly_express as px
+from dca_oop import ARPS
+st.title("Decline Curve Analysis")
+"""An app to make Decline curve analysis using ARP's models for conventional reservoirs.
+- Upload your data 
+- Specify is it field or only one well data 
+- Choose the right parameters 
+- The result will whow
+"""
+# ------------------------ side bar -------------------------
+st.sidebar.markdown("## Data Input")
+file = st.sidebar.file_uploader("Upload Data file",type=["CSV"])
+# check if the file is uploaded
+
+if file:
+    try:
+        @st.cache
+        def upload_file(file):
+            df = pd.read_csv( file , parse_dates=True )
+            return df
+        df = upload_file(file)
+        #check wheter the data if for one well or multiwell
+        type = st.sidebar.selectbox("Type of data",["One_Well","Filed_Data"])
+        if type == "Filed_Data":
+
+            # if multiwells so choose the well
+            col = st.sidebar.selectbox("Wells_name_column",list(df.columns),index=0)
+            well = st.sidebar.selectbox("Which well?",list(df[col].unique()),index=1)
+            Q_col = st.sidebar.selectbox("Production column",list(df.columns),index=2)
+            date = st.sidebar.selectbox("Date column",list(df.columns))
+            freq = st.sidebar.selectbox("data Frequency", ["Daily", "Monthly", "Yearly"])
+            # the final df of the well
+            df = df[df[col] == well]
+            arps_model = ARPS(df,Q_col,date)
+            # show data
+            """sample of data"""
+            st.write(df.head())
+        # if the data was only one well
+        if type == "One_Well":
+            # if multiwells so choose the well
+            Q_col = st.sidebar.selectbox("Production column",list(df.columns),index=0)
+            date = st.sidebar.selectbox("Date column",list(df.columns),index=1)
+            freq = st.sidebar.selectbox("data Frequency", ["Daily", "Monthly", "Yearly"])
+            # the final df of the well
+            df = df[[date,Q_col]]
+            # show data
+            """sample of data"""
+            st.write(df.head())
+
+        """## Smoothing the data using moving average"""
+        window = st.slider("Window size",min_value=10,max_value=200,value= 100)
+        std = st.slider("Removing outliers",min_value=1,max_value=10,value=3)
+        df_smoothed = arps_model.smooth(window_size=window,stds=std,trim=True)
+        Q_smoothed= Q_col+"_rol_Av"
+        df2= df_smoothed[[date,Q_col,Q_smoothed]]
+        fig = px.line(df2,x=date,y=[Q_col,Q_smoothed],width=900,height=600)
+        st.plotly_chart(fig)
+
+        # preprocess the data column
+        arps_model.prepocess_date_col(frequency=freq)
+        # show all line
+        """## ARP's models fitted"""
+        # fit all the model
+        parameters , Qs = arps_model.fit_all_models()
+        fig = px.line(Qs,x="Time",y=["Original_Smoothed","Exponential","Harmonic","Hyperbolic"],width=1000,height=600)
+        st.plotly_chart(fig)
+        """#### Models parameters"""
+        st.table(parameters)
+        st.success('Made with love'+'\u2764\ufe0f'+' Ahmed Elsayed')
+    except:
+        st.error("Make sure you choosed the right parameters")
